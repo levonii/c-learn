47b480a0144588007221ece1a70d7e51a9492882
l156521 <1565211920@qq.com>
2022-03-26 14:32:01 +0800

    循环

---
+++ Project3/Project3/text.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int line =0;
+	while (line < 30000)
+	{
+		printf("Ð´´úÂë\n");
+		line++;
+	}
+	if (line == 30000)
+	{
+		printf("³É¹¦\n");
+	}
+	return 0;
+}
