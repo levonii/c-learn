460b2ab3f89892e006b45ec2ed9a2fadacd849e5
l156521 <1565211920@qq.com>
2022-03-26 14:20:55 +0800

    简单代码

---
+++ Project2/Project2/oneclass.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+// printf("%d\n", sizeof(short));
+// printf("%d\n", sizeof(int));
+// printf("%d\n", sizeof(long));
+// printf("%d\n", sizeof(long long));
+// printf("%d\n", sizeof(float));
+//	return 0;
+//}
+
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//
+//	printf("%d", a);
+//	return 0;   //当局部变量和全局变量一样时，优先考虑局部变量
+//
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//
+//}
+//局部变量和全局变量覆盖范围不一样
+//全局变量覆盖整个工程
+int a = 100;
+//
+//int main()
+//{
+//
+//	return 0;
+//}
