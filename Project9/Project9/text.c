c39bb5411840be11767dc1199e3767397be851c0
l156521 <1565211920@qq.com>
2022-04-02 16:47:09 +0800

    4.2

---
+++ Project9/Project9/text.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include "myfun.h"
+int main(int argc, char* argv[])
+{
+	myfun1();
+	return 0;
+}
---
+++ Project9/Project9/text1.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "myfun.h"
+#include<stdio.h>
+void myfun1()
+{
+	printf("hello world");
+	return;
+}
+
