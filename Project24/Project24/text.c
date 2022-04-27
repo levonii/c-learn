087100ee1587122e2ce0bf81fc29f1f240465449
l156521 <1565211920@qq.com>
2022-04-27 21:45:26 +0800

    .

---
+++ Project24/Project24/text.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+void text1()
+{
+	int temp;
+	int* p;
+	int* q;
+	temp=
+
+	return 0;
+}
+
+int main(int argc,char *grgv[])
+{
+
+	return 0;
+}
