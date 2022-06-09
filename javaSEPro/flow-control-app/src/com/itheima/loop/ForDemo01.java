2f00b252c5ff4ff1b792acbf304a13f75b9c0519
l156521 <1565211920@qq.com>
2022-06-09 18:31:14 +0800

    for基础学习

---
+++ javaSEPro/flow-control-app/src/com/itheima/loop/ForDemo01.java
@@ -0,0 +1,15 @@
+package com.itheima.loop;
+
+public class ForDemo01 {
+    public static void main(String[] args) {
+        //目标：学会使用if语句
+        //输出三次hello world
+        int i;
+        for(i=0;i<3;i++){
+            System.out.println("hello world");
+        }
+        for(i=0;i<3;++i){
+            System.out.println("hello world");
+        }
+    }
+}
